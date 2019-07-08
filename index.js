a2586c222583901246e876eee3555efee82545fd
Puya ars <puyaars@gmail.com>
2019-07-08 17:22:59 +0430

    bug fixes

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 console.log(process.env.MONGO_URI)
+console.log(process.env.TOKEN)
 if(process.env.MONGO_URI == undefined) process.env.MONGO_URI = 'mongodb://root:VyD4h5LcmaYfpPCc69muIuFy@s7.liara.ir:30770/mrbottest'
 if(process.env.TOKEN == undefined) process.env.TOKEN = '882220621:AAEbh2pgLrq99WDKn35d7eSvymMLW1eGysM'
+
+process.env.MONGO_URI = 'mongodb://mongodb:27017/isawyou'
 
 require('./src/app')
