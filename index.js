97e5701ff63e1a4f1aae5bdd4ab50747cc499428
Puya ars <puyaars@gmail.com>
2019-07-08 17:14:49 +0430

    bug fixes

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-console.log(process.env.DB_NAME)
-console.log(process.env.TOKEN)
-
+console.log(process.env.MONGO_URI)
 if(process.env.MONGO_URI == undefined) process.env.MONGO_URI = 'mongodb://root:VyD4h5LcmaYfpPCc69muIuFy@s7.liara.ir:30770/mrbottest'
 if(process.env.TOKEN == undefined) process.env.TOKEN = '882220621:AAEbh2pgLrq99WDKn35d7eSvymMLW1eGysM'
 
